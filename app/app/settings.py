c65ea5d21582e9f793bbae4dd88c68af7d53369b
kapil <kapil.kumar.svp@gmail.com>
2024-10-11 13:29:35 +0000

    Created a custom user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Use the custom user model for all users
+AUTH_USER_MODEL = 'core.User'
