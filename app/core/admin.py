936944e3ee1332223006b927e8d4497056281a0c
kapil <kapil.kumar.svp@gmail.com>
2024-10-12 13:42:40 +0000

    Register custom user model and written test cases for it

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,19 @@
-from django.contrib import admin
+"""
+Django admin cutomization
+"""
 
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core import models
 # Register your models here.
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define admin pages for users"""
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,32 @@
+"""Test for the django admin modifications"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    """Test for django admin"""
+
+    # Create setup for runnning the test case
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com",
+            password="test@123"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='testuser@example.com',
+            password='test@123',
+            name='testuser'
+        )
+
+    def test_user_list(self):
+        """Test that users are listed on page."""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
