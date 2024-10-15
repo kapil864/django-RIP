e06e440f34bc8691fcbfe2186c35aa5e795cd773
kapil <kapil.kumar.svp@gmail.com>
2024-10-15 05:49:52 +0000

    Created a recipe app

--- app/app/settings.py
+++ app/app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'drf_spectacular',
     'user',
+    'recipe',
 ]
 
 MIDDLEWARE = [
@@ -140,5 +141,5 @@ AUTH_USER_MODEL = 'core.User'
 
 # Tell rest framework to use open api for generating schema
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS':'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
